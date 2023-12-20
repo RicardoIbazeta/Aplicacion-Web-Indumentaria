5e8fe797a154654b66203adcb4ccef1c4cddafc6
Ricardo Ibazeta <picuibazeta@gmail.com>
2023-12-20 16:15:59 -0300

    noveno commit

--- src/main/java/ProyectoIndumentaria/Servicios/ProductoServicio.java
+++ src/main/java/ProyectoIndumentaria/Servicios/ProductoServicio.java
@@ -1,6 +1,61 @@
 
 package ProyectoIndumentaria.Servicios;
 
-public class ProductoServicio {
+import ProyectoIndumentaria.Entidades.Categoria;
+import ProyectoIndumentaria.Entidades.Producto;
+import ProyectoIndumentaria.Excepciones.MiException;
+import ProyectoIndumentaria.Repositorios.ProductoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service
+public class ProductoServicio{
+    
+    
+    @Autowired
+    private ProductoRepositorio productoRepositorio;
+    
+    
+    @Transactional
+    public void crearProducto(String nombre, Double precio, Categoria categoria, Boolean altaBaja, String talle, Integer cantidad ) throws MiException{
+        
+        
+        validarProducto(nombre, precio, categoria, talle, cantidad);
+        Producto producto = new Producto();
+        producto.setNombre(nombre);
+        producto.setPrecio(precio);
+        producto.setCantidad(cantidad);
+        producto.setCategoria(categoria);
+        producto.setTalle(talle);
+        
+        producto.setAltaBaja(true);
+        
+        
+        productoRepositorio.save(producto);
+        
+        
+    }
+    
+    
+    private void validarProducto(String nombre, Double precio, Categoria categoria, String talle, Integer cantidad ) throws MiException{
+        if (nombre == null || nombre.isEmpty()) {
+            throw new MiException("Debes completar tu nombre");
+        }
+        if (precio == null) {
+            throw new MiException("Debes completar el precio");
+        }
+        if (talle == null || talle.isEmpty()) {
+            throw new MiException("Debes completar el talle");
+        }
+        if (cantidad == null ) {
+            throw new MiException("Debes completar la cantidad de productos: " + nombre);
+        }
+    }
+    
+    
+    
     
 }
