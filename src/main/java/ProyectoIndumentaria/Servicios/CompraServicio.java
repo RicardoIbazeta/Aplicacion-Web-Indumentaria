daa78878215383977947c28d63573825aace32d1
Ricardo Ibazeta <picuibazeta@gmail.com>
2023-12-20 18:27:11 -0300

    CompraServicio

--- src/main/java/ProyectoIndumentaria/Servicios/CompraServicio.java
+++ src/main/java/ProyectoIndumentaria/Servicios/CompraServicio.java
@@ -7,29 +7,27 @@ import ProyectoIndumentaria.Entidades.Producto;
 import ProyectoIndumentaria.Repositorios.CompraRepositorio;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public class CompraServicio {
     
     private CompraRepositorio compraRepositorio;
     
-    
-    public void crearCompra(Cliente cliente, List<Producto> productos ){
-        
+    @Transactional
+    public void crearCompra(Cliente cliente, Producto producto ){
         Compra compra= new Compra();
         compra.setCliente(cliente);
-        double montoTotal=0.0;
-        
-        for (Producto producto : productos) {
-            double precio=producto.getPrecio();
-            montoTotal+=precio;
-        }
-        
-        compra.setMonto(montoTotal);
+        compra.setMonto(producto.getPrecio());
         compra.setFechaCompra(new Date());
         compraRepositorio.save(compra);
     }
     
-    public 
+    public void modificarCompra(String id){
+        
+       Optional <Compra> respuesta = 
+        
+    }
     
 }
